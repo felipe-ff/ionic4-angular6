0291f43db0b2f3c1955199aa87032af3d48bc5f6
felipe <felipe.ferraz18@gmail.com>
2018-10-26 17:01:29 -0300

    Evoluindo

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+//import { AlertController } from 'ionic-angular';
 import { NgZone  } from '@angular/core';
 import { Events } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
@@ -15,15 +15,16 @@ export class ListPage {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
-  notifications = [];
+  notifications = ['teste'];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage,
-              public events: Events, private zone: NgZone) {
+              public events: Events, private zone: NgZone, //public alertCtrl: AlertController
+              ) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     this.storage.get('list').then((val) => {
-      if (val) this.notifications = val;
+      if (val && val.length > 0) this.notifications = val;
     });
 
     this.events.subscribe('updateScreen', () => {
@@ -35,15 +36,19 @@ export class ListPage {
       });
     });
 
-    notificationListener.listen((n) => {
-      if (n.text.toUpperCase().indexOf('COMPRA') >= 0) {
-        this.notifications.push(n.title + '-' + n.text);
-        storage.set('list', this.notifications);
-        events.publish('updateScreen');
-      }
-    }, function(e) {
-      console.log("Notification Error " + e);
-    });
+    try {
+      notificationListener.listen((n) => {
+        if (n.text.toUpperCase().indexOf('COMPRA') >= 0) {
+          this.notifications.push(n.title + '-' + n.text);
+          storage.set('list', this.notifications);
+          events.publish('updateScreen');
+        }
+      }, function(e) {
+        console.log("Notification Error " + e);
+      });
+    } catch(e) {
+      console.log('notificationListener und');
+    }
   }
 
   itemTapped(event, item) {
@@ -51,5 +56,15 @@ export class ListPage {
     this.navCtrl.push(ListPage, {
       item: item
     });
+  }
+
+  removeItem(event, item) {
+    console.log(item);
+    const index = this.notifications.indexOf(item, 0);
+    if (index > -1) {
+      this.notifications.splice(index, 1);
+    }
+
+    this.storage.set('list', this.notifications);
   }
 }
